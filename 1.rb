85939d4212e0ca07496d6dcd9853b42372ad2338
Vasanth Pappu <vpappu@covermymeds.com>
2015-04-21 21:23:41 -0400

    make scrolling faster

--- 1.rb
+++ 1.rb
@@ -1,4 +1,5 @@
 text = ARGV.first || "when are we working on the pairing app?"
+file = ARGV[1] || "themessage.gif"
 screenwidth = 10
 pad = screenwidth
 
@@ -9,12 +10,12 @@ loops = text.size - screenwidth + 1
 loops.times do |n|
   frametext = text.split('')[n..(n+screenwidth)]
   string = frametext.join('')
-  puts string
+  #puts string
   counter = "%04d" % n
   `convert -background lightblue -fill blue -font Courier -pointsize 40 label:"#{string}" tmp#{counter}.gif`
 end
 
-`convert -delay 20 -loop 0 tmp*.gif themessage.gif`
+`convert -delay 10 -loop 0 tmp*.gif ~/Documents/#{file}`
 
 `rm tmp*gif`
 
